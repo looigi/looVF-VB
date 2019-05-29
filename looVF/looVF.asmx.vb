e3305b01282dfdc74b19c7cc6ac1facb23bc77e0
Luigi Pecce <looigi@gmail.com>
2019-05-29 14:40:02 +0200

    Prima Versione 6

--- looVF/looVF.asmx.vb
+++ looVF/looVF.asmx.vb
@@ -12,144 +12,241 @@ Public Class looVF
 	Inherits System.Web.Services.WebService
 
 	<WebMethod()>
-	Public Function CreaDirectoryVirtuale(App As String, Path As String) As String
-		Dim IIs As New IIS
-		' IIs.CreateVirtualDir("LocalHost", "looVF", "Appoggio", "C:\Appoggio")
-		IIs.CreateVirtualDir("Default Web Site", "Appoggi2", "D:\Appoggio")
+	Public Function RitornaCategorie() As String
+		Dim Db As New GestioneDB
+		Dim Ritorno As String = ""
+		Dim Sql As String
+
+		If Db.LeggeImpostazioniDiBase() = True Then
+			Dim ConnSQL As Object = Db.ApreDB()
+			Dim Rec As Object = CreateObject("ADODB.Recordset")
+
+			Sql = "Select * From Categorie"
+			Rec = Db.LeggeQuery(ConnSQL, Sql)
+			Do Until Rec.Eof
+				Ritorno = Rec("idCategoria").Value & ";" & Rec("idTipologia").Value & ";" & Rec("Categoria").Value.ToString.Replace(";", "***PV***") & ";§"
+
+				Rec.MoveNext
+			Loop
+			Rec.Close()
+		End If
+
+		Return Ritorno
 	End Function
 
 	<WebMethod()>
-	Public Function RitornaFiles(Aggiorna As String) As String
+	Public Function RitornaQuantiFiles(idTipologia As String) As String
+		Dim Db As New GestioneDB
+		Dim Quanti As String = ""
+		Dim Sql As String
+
+		If Db.LeggeImpostazioniDiBase() = True Then
+			Dim ConnSQL As Object = Db.ApreDB()
+			Dim Rec As Object = CreateObject("ADODB.Recordset")
+
+			Sql = "Select Count(*) From Dati Where idTipologia=" & idTipologia
+			Rec = Db.LeggeQuery(ConnSQL, Sql)
+			Quanti = Rec(0).Value
+			Rec.Close()
+		End If
+
+		Return Quanti
+	End Function
+
+	<WebMethod()>
+	Public Function RitornMultimediaDaId(Tipologia As String, idMultimedia As String) As String
+		Dim Db As New GestioneDB
+		Dim Ritorno As String = ""
+		Dim Sql As String
+
+		If Db.LeggeImpostazioniDiBase() = True Then
+			Dim ConnSQL As Object = Db.ApreDB()
+			Dim Rec As Object = CreateObject("ADODB.Recordset")
+
+			Sql = "Select * From Dati Where idTipologia=" & Tipologia & " And Progressivo=" & idMultimedia
+			Rec = Db.LeggeQuery(ConnSQL, Sql)
+			If Not Rec.Eof Then
+				Ritorno = Rec("NomeFile").Value.ToString.Replace(";", "***PV***") & ";" & Rec("Dimensioni").Value & ";" & Rec("Data").Value & ";" & Rec("idCategoria").Value & ";"
+			Else
+				Ritorno = "ERROR: Nessun file rilevato"
+			End If
+			Rec.Close()
+		End If
+
+		Return Ritorno
+	End Function
+
+	<WebMethod()>
+	Public Function RitornaFiles() As String
 		Dim gf As New GestioneFilesDirectory
 
-		If Aggiorna = "N" Then
-			Dim Ok As Boolean = False
+		'If Aggiorna = "N" Then
+		'	Dim Ok As Boolean = False
 
-			gf.ScansionaDirectorySingola(Server.MapPath(".") & "\Temp\")
-			Dim Filetti() As String = gf.RitornaFilesRilevati
-			Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
-			Dim MaxDatella As Date = "01/01/1970 00:00:00"
-			For i As Integer = 1 To qFiles
-				Dim Nome As String = gf.TornaNomeFileDaPath(Filetti(i))
-				Dim Campi() As String = Nome.Split("_")
-				Nome = Campi(0) & "/" & Campi(1) & "/" & Campi(2) & " " & Campi(3) & ":" & Campi(4) & ":" & Campi(5)
-				Dim Datella As Date = Nome.Replace(".txt", "")
-				If Datella > MaxDatella Then
-					MaxDatella = Datella
-					Ok = True
+		'	gf.ScansionaDirectorySingola(Server.MapPath(".") & "\Temp\")
+		'	Dim Filetti() As String = gf.RitornaFilesRilevati
+		'	Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
+		'	Dim MaxDatella As Date = "01/01/1970 00:00:00"
+		'	For i As Integer = 1 To qFiles
+		'		Dim Nome As String = gf.TornaNomeFileDaPath(Filetti(i))
+		'		Dim Campi() As String = Nome.Split("_")
+		'		Nome = Campi(0) & "/" & Campi(1) & "/" & Campi(2) & " " & Campi(3) & ":" & Campi(4) & ":" & Campi(5)
+		'		Dim Datella As Date = Nome.Replace(".txt", "")
+		'		If Datella > MaxDatella Then
+		'			MaxDatella = Datella
+		'			Ok = True
+		'		End If
+		'	Next
+
+		'	If Ok Then
+		'		Dim RitornoAggiorna As String = Server.MapPath(".") & "\Temp\" & MaxDatella.ToString.Replace(":", "_").Replace(" ", "_").Replace("\", "_").Replace("/", "_") & ".txt"
+
+		'		Return RitornoAggiorna
+		'	End If
+		'End If
+
+		Dim sPathsVideo As String = gf.LeggeFileIntero(Server.MapPath(".") & "\PercorsiVideo.txt")
+		Dim sPathsImm As String = gf.LeggeFileIntero(Server.MapPath(".") & "\PercorsiImmagini.txt")
+		Dim PathVideo() As String = sPathsVideo.Split("§")
+		Dim PathImmagini() As String = sPathsImm.Split("§")
+		'Dim FilesVideo As List(Of StrutturaFiles) = New List(Of StrutturaFiles)
+		'Dim FilesImmagini As List(Of StrutturaFiles) = New List(Of StrutturaFiles)
+		Dim Conta As Long
+		Dim Db As New GestioneDB
+		Dim Sql As String
+
+		If Db.LeggeImpostazioniDiBase() = True Then
+			Dim ConnSQL As Object = Db.ApreDB()
+			Dim idCategoria As Integer = 0
+
+			Db.EsegueSql(ConnSQL, "Delete From Categorie")
+			Db.EsegueSql(ConnSQL, "Delete From Dati")
+
+			Conta = 0
+			For Each p As String In PathVideo
+				If p.Trim <> "" Then
+					Dim pp() As String = p.Split(";")
+
+					idCategoria += 1
+					Sql = "Insert Into Categorie Values (" & idCategoria & ", 2, '" & pp(0).Replace("'", "''") & "', '" & pp(1).Replace("'", "''") & "')"
+					Dim sRitorno As String = Db.EsegueSql(ConnSQL, Sql)
+
+					If Strings.Right(pp(1), 1) <> "\" Then
+						pp(1) &= "\"
+					End If
+					gf.ScansionaDirectorySingola(pp(1))
+					Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
+					Dim Files() As String = gf.RitornaFilesRilevati
+					For i As Integer = 1 To qFiles
+						'Dim sf As New StrutturaFiles
+
+						'sf.Categoria = Conta
+						'sf.NomeFile = Files(i).Replace(pp(1), "").Replace(";", "**PV***").Replace("§", "***COSO***")
+						'sf.DimensioniFile = FileLen(Files(i))
+						'sf.DataFile = FileDateTime(Files(i))
+
+						'FilesVideo.Add(sf)
+						Conta += 1
+						Sql = "Insert Into Dati Values (" &
+							" " & Conta & ", " &
+							"2, " &
+							" " & idCategoria & ", " &
+							"'" & Files(i).Replace(pp(1), "").Replace("'", "''") & "', " &
+							" " & FileLen(Files(i)) & ", " &
+							"'" & FileDateTime(Files(i)) & "' " &
+							")"
+						sRitorno = Db.EsegueSql(ConnSQL, Sql)
+						If sRitorno <> "" Then
+							Return "ERROR:" & sRitorno
+						End If
+					Next
 				End If
 			Next
 
-			If Ok Then
-				Dim RitornoAggiorna As String = Server.MapPath(".") & "\Temp\" & MaxDatella.ToString.Replace(":", "_").Replace(" ", "_").Replace("\", "_").Replace("/", "_") & ".txt"
+			Conta = 0
+			idCategoria = 0
+			For Each p As String In PathImmagini
+				If p.Trim <> "" Then
+					Dim pp() As String = p.Split(";")
 
-				Return RitornoAggiorna
-			End If
+					idCategoria += 1
+					Sql = "Insert Into Categorie Values (" & idCategoria & ", 1, '" & pp(0).Replace("'", "''") & "', '" & pp(1).Replace("'", "''") & "')"
+					Dim sRitorno As String = Db.EsegueSql(ConnSQL, Sql)
+
+					If Strings.Right(pp(1), 1) <> "\" Then
+						pp(1) &= "\"
+					End If
+					gf.ScansionaDirectorySingola(pp(1))
+					Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
+					Dim Files() As String = gf.RitornaFilesRilevati
+					' Conta += 1
+					For i As Integer = 1 To qFiles
+						'Dim sf As New StrutturaFiles
+
+						'sf.Categoria = Conta
+						'sf.NomeFile = Files(i).Replace(pp(1), "").Replace(";", "**PV***").Replace("§", "***COSO***")
+						'sf.DimensioniFile = FileLen(Files(i))
+						'sf.DataFile = FileDateTime(Files(i))
+
+						'FilesImmagini.Add(sf)
+						Conta += 1
+						Sql = "Insert Into Dati Values (" &
+							" " & Conta & ", " &
+							"1, " &
+							" " & idCategoria & ", " &
+							"'" & Files(i).Replace(pp(1), "").Replace("'", "''") & "', " &
+							" " & FileLen(Files(i)) & ", " &
+							"'" & FileDateTime(Files(i)) & "' " &
+							")"
+						sRitorno = Db.EsegueSql(ConnSQL, Sql)
+						If sRitorno <> "" Then
+							Return "ERROR: " & sRitorno
+						End If
+					Next
+				End If
+			Next
+
+			'gf.CreaDirectoryDaPercorso(Server.MapPath(".") & "\Temp\")
+
+			'Dim NomeFile As String = Server.MapPath(".") & "\Temp\" & Now.ToString.Replace(":", "_").Replace(" ", "_").Replace("\", "_").Replace("/", "_") & ".txt"
+			'gf.ApreFileDiTestoPerScrittura(NomeFile)
+
+			'For Each p As String In PathVideo
+			'	If p.Trim <> "" Then
+			'		Dim pp() As String = p.Split(";")
+			'		Dim Stringa As String = "CategoriaVideo;" & pp(0).Replace(";", "**PV***").Replace("§", "***COSO***") & ";§"
+			'		gf.ScriveTestoSuFileAperto(Stringa)
+			'	End If
+			'Next
+
+			'For Each p As String In PathImmagini
+			'	If p.Trim <> "" Then
+			'		Dim pp() As String = p.Split(";")
+			'		Dim Stringa As String = "CategoriaImmagini;" & pp(0).Replace(";", "**PV***").Replace("§", "***COSO***") & ";§"
+			'		gf.ScriveTestoSuFileAperto(Stringa)
+			'	End If
+			'Next
+
+			'For Each sf As StrutturaFiles In FilesVideo
+			'Dim Stringa As String = "Video;" & sf.Categoria & ";" & sf.NomeFile & ";" & sf.DimensioniFile.ToString & ";" & sf.DataFile & ";§"
+			'gf.ScriveTestoSuFileAperto(Stringa)
+			'Next
+
+			'For Each sf As StrutturaFiles In FilesImmagini
+			'Dim Stringa As String = "Pic;" & sf.Categoria & ";" & sf.NomeFile & ";" & sf.DimensioniFile.ToString & ";" & sf.DataFile & ";§"
+			'gf.ScriveTestoSuFileAperto(Stringa)
+			'Next
+			'gf.ChiudeFileDiTestoDopoScrittura()
+
+			' Dim Ritorno As String = gf.LeggeFileIntero(NomeFile)
+
+			gf = Nothing
+
+			'Dim Ritorno As String = NomeFile
 		End If
 
-		Dim sPathsVideo As String = gf.LeggeFileIntero(Server.MapPath(".") & "\PercorsiVideo.txt")
-		'If sPathsVideo = "" Then
-		'	Return "ERROR: Nessun path video presente"
-		'End If
-		Dim sPathsImm As String = gf.LeggeFileIntero(Server.MapPath(".") & "\PercorsiImmagini.txt")
-		'If sPathsImm = "" Then
-		'	Return "ERROR: Nessun path immagini presente"
-		'End If
-		Dim PathVideo() As String = sPathsVideo.Split("§")
-		Dim PathImmagini() As String = sPathsImm.Split("§")
-		Dim FilesVideo As List(Of StrutturaFiles) = New List(Of StrutturaFiles)
-		Dim FilesImmagini As List(Of StrutturaFiles) = New List(Of StrutturaFiles)
-		Dim Conta As Integer
 
-		Conta = 0
-		For Each p As String In PathVideo
-			If p.Trim <> "" Then
-				Dim pp() As String = p.Split(";")
-
-				If Strings.Right(pp(1), 1) <> "\" Then
-					pp(1) &= "\"
-				End If
-				gf.ScansionaDirectorySingola(pp(1))
-				Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
-				Dim Files() As String = gf.RitornaFilesRilevati
-				Conta += 1
-				For i As Integer = 1 To qFiles
-					Dim sf As New StrutturaFiles
-
-					sf.Categoria = Conta
-					sf.NomeFile = Files(i).Replace(pp(1), "").Replace(";", "**PV***").Replace("§", "***COSO***")
-					sf.DimensioniFile = FileLen(Files(i))
-					sf.DataFile = FileDateTime(Files(i))
-
-					FilesVideo.Add(sf)
-				Next
-			End If
-		Next
-
-		Conta = 0
-		For Each p As String In PathImmagini
-			If p.Trim <> "" Then
-				Dim pp() As String = p.Split(";")
-
-				If Strings.Right(pp(1), 1) <> "\" Then
-					pp(1) &= "\"
-				End If
-				gf.ScansionaDirectorySingola(pp(1))
-				Dim qFiles As Integer = gf.RitornaQuantiFilesRilevati
-				Dim Files() As String = gf.RitornaFilesRilevati
-				Conta += 1
-				For i As Integer = 1 To qFiles
-					Dim sf As New StrutturaFiles
-
-					sf.Categoria = Conta
-					sf.NomeFile = Files(i).Replace(pp(1), "").Replace(";", "**PV***").Replace("§", "***COSO***")
-					sf.DimensioniFile = FileLen(Files(i))
-					sf.DataFile = FileDateTime(Files(i))
-
-					FilesImmagini.Add(sf)
-				Next
-			End If
-		Next
-
-		gf.CreaDirectoryDaPercorso(Server.MapPath(".") & "\Temp\")
-
-		Dim NomeFile As String = Server.MapPath(".") & "\Temp\" & Now.ToString.Replace(":", "_").Replace(" ", "_").Replace("\", "_").Replace("/", "_") & ".txt"
-		gf.ApreFileDiTestoPerScrittura(NomeFile)
-
-		For Each p As String In PathVideo
-			If p.Trim <> "" Then
-				Dim pp() As String = p.Split(";")
-				Dim Stringa As String = "CategoriaVideo;" & pp(0).Replace(";", "**PV***").Replace("§", "***COSO***") & ";§"
-				gf.ScriveTestoSuFileAperto(Stringa)
-			End If
-		Next
-
-		For Each p As String In PathImmagini
-			If p.Trim <> "" Then
-				Dim pp() As String = p.Split(";")
-				Dim Stringa As String = "CategoriaImmagini;" & pp(0).Replace(";", "**PV***").Replace("§", "***COSO***") & ";§"
-				gf.ScriveTestoSuFileAperto(Stringa)
-			End If
-		Next
-
-		For Each sf As StrutturaFiles In FilesVideo
-			Dim Stringa As String = "Video;" & sf.Categoria & ";" & sf.NomeFile & ";" & sf.DimensioniFile.ToString & ";" & sf.DataFile & ";§"
-			gf.ScriveTestoSuFileAperto(Stringa)
-		Next
-
-		For Each sf As StrutturaFiles In FilesImmagini
-			Dim Stringa As String = "Pic;" & sf.Categoria & ";" & sf.NomeFile & ";" & sf.DimensioniFile.ToString & ";" & sf.DataFile & ";§"
-			gf.ScriveTestoSuFileAperto(Stringa)
-		Next
-		gf.ChiudeFileDiTestoDopoScrittura()
-
-		' Dim Ritorno As String = gf.LeggeFileIntero(NomeFile)
-
-		gf = Nothing
-
-		Dim Ritorno As String = NomeFile
-
-		Return Ritorno
+		Return "*"
 	End Function
 
 End Class
